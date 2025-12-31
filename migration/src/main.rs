a3e0496b098bf5fe3ab5ed91a91d7996d6bd2888
ni <nicolas.nkruger@gmail.com>
2025-12-31 18:47:14 -0300

    migration_sample, docker-compose

---
+++ migration/src/main.rs
@@ -0,0 +1,20 @@
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgresql://postgres:postgres@localhost:5432/mydb")
+        .await?;
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await?;
+
+    print!("{}", row.0);
+
+    assert_eq!(row.0, 150);
+
+    Ok(())
+}
