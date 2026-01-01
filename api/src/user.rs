66cf6db0ee0031033c491d098b38350a161e80dd
nicolas kruger <nicolas.nkruger@gmail.com>
2026-01-01 17:57:21 -0300

    do not create repeat user

--- api/src/user.rs
+++ api/src/user.rs
@@ -11,3 +11,61 @@ pub struct UserDto {
 pub async fn create(body: web::Json<UserDto>) -> impl Responder {
     HttpResponse::Created().body(body.name.clone())
 }
+
+trait IUserRepo {
+    async fn exists(&self, name: &String) -> bool;
+}
+
+trait IUserService {
+    async fn register(&self, dto: &UserDto) -> Result<(), String>;
+}
+
+struct UserService<R: IUserRepo> {
+    repo: R,
+}
+
+impl<R: IUserRepo> UserService<R> {
+    fn new(repo: R) -> Self {
+        Self { repo }
+    }
+}
+
+impl<R: IUserRepo> IUserService for UserService<R> {
+    async fn register(&self, dto: &UserDto) -> Result<(), String> {
+        if self.repo.exists(&dto.name).await {
+            Err(String::from("Not created because it already exists."))
+        } else {
+            Ok(())
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockUserRepo {
+        mock_exists: bool,
+    }
+
+    impl IUserRepo for MockUserRepo {
+        async fn exists(&self, name: &String) -> bool {
+            self.mock_exists
+        }
+    }
+
+    #[tokio::test]
+    async fn error_on_create_existing_user() {
+        let mock_repo = MockUserRepo { mock_exists: true };
+
+        let service = UserService { repo: mock_repo };
+
+        let dto = UserDto {
+            name: String::from("nk"),
+            password: String::from("123"),
+        };
+
+        let result = service.register(&dto).await;
+
+        assert!(result.is_err(), "Not created because it already exists.");
+    }
+}
