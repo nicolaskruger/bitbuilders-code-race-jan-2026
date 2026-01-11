7c9c112d8722081dab2de0516b17f192a8e94139
nicolas kruger <nicolas.nkruger@gmail.com>
2026-01-11 14:06:43 -0300

    reafator

--- api/src/contract/service/auth_service_trait.rs
+++ api/src/contract/service/auth_service_trait.rs
@@ -1,7 +1,9 @@
 use actix_web::{HttpRequest, http::StatusCode};
+use async_trait::async_trait;
 
 use crate::entity::auth_entity::{AuthResponse, LoggedUser, UserAuth};
 
+#[async_trait]
 pub trait IAuthService {
     async fn auth(&self, user: &UserAuth) -> Result<AuthResponse, String>;
     async fn user(&self, req: HttpRequest) -> Result<LoggedUser, StatusCode>;
