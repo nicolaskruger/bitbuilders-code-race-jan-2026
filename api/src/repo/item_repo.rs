7349f8f8f56e900fac0db4113acf30b7356ca650
nicolas kruger <nicolas.nkruger@gmail.com>
2026-01-12 06:27:56 -0300

    item, wip

--- api/src/repo/item_repo.rs
+++ api/src/repo/item_repo.rs
@@ -1,1 +1,24 @@
+use async_trait::async_trait;
+use sqlx::{Pool, Postgres};
 
+use crate::{
+    contract::repo::{item_repo_trait::IItemRepo, user_repo_trait::IUserRepo},
+    entity::item_entity::ItemEntity,
+};
+
+pub struct ItemRepo<'a> {
+    pool: &'a Pool<Postgres>,
+}
+
+impl<'a> ItemRepo<'a> {
+    pub fn new(pool: &'a Pool<Postgres>) -> Self {
+        Self { pool }
+    }
+}
+
+#[async_trait]
+impl IItemRepo for ItemRepo<'_> {
+    async fn create(&self, name: &ItemEntity) -> bool {
+        todo!()
+    }
+}
--- api/src/repo/mod.rs
+++ api/src/repo/mod.rs
@@ -1,1 +1,2 @@
+pub mod item_repo;
 pub mod user_repo;
